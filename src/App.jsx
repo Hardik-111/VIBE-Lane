655e17ba0613fbdc9aa4974ab4359f3aa250dff5
Hardik <ihardik.112@gmail.com>
2023-04-08 08:04:25 +0530

    created Cart desgin

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return <Login/>;
+  // return <Login/>;
+  // return <ProductList/>;
+  // return <Product/>;
+  return <Home/>;
+  // return <Cart/>;
 };
 
 export default App;
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,12 +4,13 @@ import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
+import { mobile } from "../responsive";
 
 const Container=styled.div``;
 const Wrapper=styled.div`
     padding:50px;
     display:flex;
-
+    ${mobile({ padding:"10px", flexDirection:"column" } )}
 `;
 const ImgContainer=styled.div`
     flex:1;
@@ -17,10 +18,15 @@ const ImgContainer=styled.div`
 const Image=styled.img`
     width:100%;
     height:90vh;
+    object-fit: cover;
+    ${mobile({ height:"40vh" } )}
+
 `;
 const InfoContainer=styled.div`
     flex:1;
     padding: 0px 50px ;
+    ${mobile({ padding:"10px" } )}
+    
 `;
 const Title=styled.h1`
     font-weight:200;
@@ -39,7 +45,8 @@ const FilterContainer=styled.div`
     margin:30px 0px;
     display: flex;
     justify-content:space-between;
-
+    ${mobile({ width:"100%" } )}
+    
 `;
 const Filter=styled.div`
     display:flex;;
@@ -68,6 +75,8 @@ const AddContainer=styled.div`
     display:flex;
     align-items:center;
     justify-content:space-between; 
+    ${mobile({ width:"100%" } )}
+
 `;
 const AmountContainer=styled.div`
     display:flex;
@@ -102,7 +111,7 @@ const Product = () => {
       <Announcement/>
       <Wrapper>
         <ImgContainer>
-            <Image src="https://i.ibb.co/S6qMxwr/jean.jpg"/>
+            <Image src="https://assets.ajio.com/medias/sys_master/root/20230302/nrcw/6400901eaeb26924e3a02a48/-1117Wx1400H-441142426-ltblue-MODEL.jpg"/>
         </ImgContainer>
         <InfoContainer>
             <Title>Denim Jumpsuit</Title>
