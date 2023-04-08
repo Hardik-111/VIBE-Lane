7e6f318cb18faebf00c49fc583f60896b8fdf256
Hardik <ihardik.112@gmail.com>
2023-04-08 08:04:25 +0530

    created Cart design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from "@material-ui/icons";
 import styled from "styled-components"
+import { mobile } from "../responsive";
 
 const Container=styled.div`
   display:flex;
-
+  ${mobile({ flexDirection: "column" } )}
 `;
 const Left=styled.div`
   flex:1;
@@ -38,7 +39,7 @@ const SocialIcon=styled.div`
 const Center=styled.div`
   flex:1;
   padding:20px;
-
+  ${mobile({ display:"none" } )}
 `;
 
 const Title=styled.h3`
@@ -62,6 +63,7 @@ const ListItem=styled.li`
 const Right=styled.div`
   flex:1;
   padding:20px;
+  ${mobile({ backgroundColor: "#fff8f8" } )}
 `;
 
 const ContactItem=styled.div`
