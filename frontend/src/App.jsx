2f1e0aab71b3e3dc0891214a40bb8dd8e37513b1
Hardik <ihardik.112@gmail.com>
2023-04-09 00:12:47 +0530

    created logic to fetch and filter products

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,42 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Success from "./pages/Success";
+import { useSelector } from "react-redux";
 
 const App = () => {
-  // return <Login/>;
-  // return <Register/>;
-  // return <ProductList/>;
-  // return <Product/>;
-  // return <Home/>;
-  // return <Cart/>;
+   const user = useSelector((state) => state.user.currentUser);
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/success">
+          <Success />
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
--- frontend/src/pages/ProductList.jsx
+++ frontend/src/pages/ProductList.jsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
 import Products from '../components/Products';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
 import { mobile } from '../responsive';
+import { useLocation } from 'react-router-dom';
 
 const Container=styled.div`
 
@@ -36,28 +37,42 @@ const Select=styled.select`
 `;
 const Option=styled.option``;
 const ProductList = () => {
+  const location = useLocation();
+  const cat = location.pathname.split("/")[2];
+  const[filters, setFilters] = useState({});
+  const[sort, setSort] = useState("newest");
+  
+  const handleFilters = (e) =>{
+    const value = e.target.value;
+    setFilters({
+        ...filters, 
+        [e.target.name]: value,
+    });
+  };
+
+   
   return (
     <Container>
       <Navbar/>
       <Announcement/>
-      <Title>Dresses</Title>
+      <Title>{cat}</Title>
       <FilterContainer>
         <Filter>
         <FilterText>Filter Products:</FilterText>
-        <Select>
-            <Option disabled selected>
+        <Select name="color" onChange={handleFilters}>
+            <Option disabled >
                 Choose Color
             </Option>
-            <Option>White</Option>
-            <Option>Black</Option>
-            <Option>Red</Option>
-            <Option>Blue</Option>
-            <Option>Yellow</Option>
-            <Option>Green</Option>
+            <Option>white</Option>
+            <Option>black</Option>
+            <Option>red</Option>
+            <Option>blue</Option>
+            <Option>yellow</Option>
+            <Option>green</Option>
         </Select>
 
-        <Select>
-            <Option disabled selected>
+        <Select name="size" onChange={handleFilters}>
+            <Option disabled >
                 Size
             </Option>
             <Option>XS</Option>
@@ -70,20 +85,18 @@ const ProductList = () => {
         </Filter>
         <Filter>
         <FilterText>Sort Products:</FilterText>
-        <Select>
-            <Option selected>
-                Neweset
-            </Option>
-            <Option>Price (asc)</Option>
-            <Option>Price (desc)</Option>
+        <Select onChange={e=>setSort(e.target.value)}>
+            <Option value="newest">Newest</Option>
+            <Option value="asc">Price (asc)</Option>
+            <Option value="desc">Price (desc)</Option>
         </Select>
         </Filter>
       </FilterContainer>
-      <Products/>
+      <Products cat={cat} filters={filters} sort={sort}/>
       <Newsletter/>
       <Footer/>
     </Container>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
