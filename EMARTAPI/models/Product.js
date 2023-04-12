df6e15999f9f328df3f2e27d5fb41cd504f63804
Akash Gupta <akash1128355@gmail.com>
2023-04-12 15:33:00 +0530

    Refactored changes , Added firebase

--- EMARTAPI/models/Product.js
+++ EMARTAPI/models/Product.js
@@ -9,9 +9,9 @@ const ProductSchema = new mongoose.Schema(
     size: { type: Array},
     color: { type: Array},
     price: { type: Number, required: true },
-    
+    inStock: { type: Boolean, default: true }
   },
   { timestamps: true }
-);
+); 
 
 module.exports = mongoose.model("Product", ProductSchema);
---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2gNtzNS64lr4ZEWw4gJsTsYZtbik-3eE",
+  authDomain: "emart-923c9.firebaseapp.com",
+  projectId: "emart-923c9",
+  storageBucket: "emart-923c9.appspot.com",
+  messagingSenderId: "985715121317",
+  appId: "1:985715121317:web:0851251527f6529e03014c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
